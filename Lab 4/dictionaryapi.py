de2f004eeb2bf19cb6c1ba33335144ab25d53e98
jjmecca <46037675+jjmecca@users.noreply.github.com>
2024-03-10 18:04:37 -0400

    lab4

--- Lab 4/dictionaryapi.py
+++ Lab 4/dictionaryapi.py
@@ -1,11 +1,11 @@
 import requests
 
 class DictionaryWrapper:
-    def __init__(self, api_key):
-        self.api_key=api_key
+    def __init__(self):
         self.base_url="https://api.dictionaryapi.dev/api/v2/entries/en/"
     def definition(self, word):
         url=self.base_url+word
         response=requests.get(url)
         data=response.json()
-        print(data)
+        return data[0]['meanings'][0]['definitions'][0]['definition']
+print(DictionaryWrapper().definition("balls"))
