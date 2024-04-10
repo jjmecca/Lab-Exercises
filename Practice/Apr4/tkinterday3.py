ba3ee40c292aae2aa470dbfd83b21c48f9f7ee97
jjmecca <46037675+jjmecca@users.noreply.github.com>
2024-04-10 15:50:53 -0400

    Update tkinterday3.py

--- Practice/Apr4/tkinterday3.py
+++ Practice/Apr4/tkinterday3.py
@@ -3,7 +3,7 @@ import json
 from tkinter import *
 
 root = Tk()
-root.geometry("900x600") 
+root.geometry("570x500") 
 root.title("Weather App")
 
 lat = StringVar()
@@ -47,7 +47,7 @@ city_latlon = {"Binghamton": ["42.09", "-75.91"],
               "Chicago": ["41.98", "-87.9"],
               "Atlanta": ["33.66", "-84.42"],
               "Denver": ["39.87", "-104.67"]}
-
+'''
 label_lat = Label(root, text="Enter Latitude")
 entry_lat = Entry(root, width=35, borderwidth=5, textvariable=lat)
 label_lon = Label(root, text="Enter Longitude")
@@ -63,6 +63,7 @@ dropdown = OptionMenu(root, city_select, *city_latlon)
 choose_button = Button(root, text="Select", command=city_choice)
 test_widget = Text(root, font = ("Helvitica", "16"),
                   height=10, width=25)
+
 dropdown.pack(pady=20)
 label_lat.pack()
 entry_lat.pack()
@@ -72,5 +73,32 @@ choose_button.pack(pady=10)
 forecast_button.pack(pady=10)
 test_widget.pack()
 close_button.pack(pady=10)
+'''
+
+
+entry_frame = Frame(root)
+entry_frame.grid(row=0, column=0)
+drop_frame = Frame(root)
+drop_frame.grid(row=0, column=1)
+
+label_lat = Label(entry_frame , text="Enter Latitude").pack()
+entry_lat = Entry(entry_frame , width=35, borderwidth=5, textvariable=lat).pack()
+label_lon = Label(entry_frame , text="Enter Longitude").pack()
+entry_lon = Entry(entry_frame , width=35, borderwidth=5, textvariable=lon).pack()
+button = Button(entry_frame , font = 24, text = "Get Forecast", 
+                command=lambda: weekly_forecast(lat.get(), lon.get()))
+button.pack(pady = 20)
+
+drop_label = Label(drop_frame, text="You can also select a city:").pack()
+dropdown = OptionMenu(drop_frame, city_select, *city_list).pack()
+choose_button = Button(drop_frame, text="Select City", command=city_choice)
+choose_button.pack(pady = 20)
+
+weather_text = Text(root, height=16)
+weather_text.grid(columnspan=2)
+
+button2 = Button(root, font = 24, text = "Close Window", 
+                command=root.destroy)
+button2.grid(columnspan=2, pady = 20)
 
 root.mainloop()
